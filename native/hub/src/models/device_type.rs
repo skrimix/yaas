8542efac2645a5acda573530e5a1d6ac6cebf567
skrimix <git@skrimix.8shield.net>
2025-08-03 11:37:42 +0500

    Add q3s xbox edition

--- native/hub/src/models/device_type.rs
+++ native/hub/src/models/device_type.rs
@@ -9,6 +9,7 @@ pub enum DeviceType {
     Quest2,
     Quest3,
     Quest3S,
+    Quest3SXbox,
     QuestPro,
     Unknown,
 }
@@ -22,6 +23,7 @@ impl Display for DeviceType {
                 Self::Quest => "Meta Quest",
                 Self::Quest2 => "Meta Quest 2",
                 Self::Quest3S => "Meta Quest 3S",
+                Self::Quest3SXbox => "Meta Quest 3S Xbox Edition",
                 Self::QuestPro => "Meta Quest Pro",
                 Self::Quest3 => "Meta Quest 3",
                 Self::Unknown => "Unknown",
@@ -37,8 +39,8 @@ impl DeviceType {
             "hollywood" => DeviceType::Quest2,
             "eureka" => DeviceType::Quest3,
             "panther" => DeviceType::Quest3S,
+            "xse_panther" => DeviceType::Quest3SXbox,
             "seacliff" => DeviceType::QuestPro,
-            // TODO: add user setting for allowing unknown devices
             _ => DeviceType::Unknown,
         }
     }

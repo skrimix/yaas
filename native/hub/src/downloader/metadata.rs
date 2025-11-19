30acb93862721b3888a7886af45b9d6f1d1d9363
skrimix <git@skrimix.8shield.net>
2025-11-19 15:49:39 +0500

    Fix LastUpdated format in legacy matadata, ignore errors

--- native/hub/src/downloader/metadata.rs
+++ native/hub/src/downloader/metadata.rs
@@ -1,6 +1,10 @@
-use std::path::{Path, PathBuf};
+use std::{
+    error::Error,
+    path::{Path, PathBuf},
+};
 
 use anyhow::{Context, Result};
+use lazy_regex::lazy_regex;
 use time::{OffsetDateTime, format_description::well_known::Rfc3339};
 use tracing::{info, instrument, warn};
 
@@ -17,6 +21,7 @@ struct DownloadMetadata {
     package_name: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     version_code: Option<u32>,
+    // TODO: Should we parse this?
     #[serde(skip_serializing_if = "Option::is_none")]
     last_updated: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -38,7 +43,11 @@ pub(super) async fn write_download_metadata(
     dst_dir: &PathBuf,
     write_legacy_release: bool,
 ) -> Result<()> {
-    let now = OffsetDateTime::now_utc().format(&Rfc3339).unwrap_or_else(|_| "".to_string());
+    let now = OffsetDateTime::now_utc()
+        .replace_millisecond(0)
+        .unwrap()
+        .format(&Rfc3339)
+        .unwrap_or_else(|_| "".to_string());
 
     let meta = DownloadMetadata {
         format_version: 1,
@@ -71,27 +80,39 @@ pub(super) async fn write_download_metadata(
                 #[serde(rename = "VersionCode")]
                 version_code: u32,
                 #[serde(rename = "LastUpdated")]
-                last_updated: &'a str,
+                last_updated: String,
                 #[serde(rename = "GameSize")]
                 game_size: u64,
             }
 
-            let size_mb = app.size / 1_000_000;
-            let legacy = LegacyReleaseJson {
-                game_name: &app.app_name,
-                release_name: app_full_name.to_string(),
-                package_name: &app.package_name,
-                version_code: app.version_code,
-                last_updated: &app.last_updated,
-                game_size: size_mb,
-            };
+            if let Err(e) = async {
+                let size_mb = app.size / 1_000_000;
+                let last_updated = convert_legacy_last_updated(&app.last_updated)?;
+                let legacy = LegacyReleaseJson {
+                    game_name: &app.app_name,
+                    release_name: app_full_name.to_string(),
+                    package_name: &app.package_name,
+                    version_code: app.version_code,
+                    last_updated,
+                    game_size: size_mb,
+                };
 
-            let legacy_json = serde_json::to_string_pretty(&legacy)?;
-            let legacy_path = dst_dir.join("release.json");
-            tokio::fs::write(&legacy_path, legacy_json)
-                .await
-                .with_context(|| format!("Failed to write {}", legacy_path.display()))?;
-            info!(path = %legacy_path.display(), "Wrote legacy release.json metadata");
+                let legacy_json = serde_json::to_string_pretty(&legacy)?;
+                let legacy_path = dst_dir.join("release.json");
+                tokio::fs::write(&legacy_path, legacy_json)
+                    .await
+                    .with_context(|| format!("Failed to write {}", legacy_path.display()))?;
+
+                info!(path = %legacy_path.display(), "Wrote legacy release.json metadata");
+                Ok::<_, anyhow::Error>(())
+            }
+            .await
+            {
+                warn!(
+                    error = e.as_ref() as &dyn Error,
+                    app_full_name, "Failed to write legacy release.json"
+                );
+            }
         } else {
             warn!(app_full_name, "Could not write legacy release.json: app not found in cache");
         }
@@ -137,12 +158,33 @@ pub(crate) async fn read_download_metadata(dir: &Path) -> Result<DownloadMetadat
             // Only our metadata has downloaded_at, rename to metadata.json to avoid conflicts with QL
             let new_path = dir.join("metadata.json");
             if let Err(e) = tokio::fs::rename(&meta_path_alt, &new_path).await {
-                warn!(error = %e, "Failed to rename our release.json to metadata.json");
+                warn!(
+                    error = &e as &dyn Error,
+                    "Failed to rename our release.json to metadata.json"
+                );
             }
         }
     }
 
     Ok(DownloadMetadataInfo { downloaded_at: ts_millis, package_name, version_code })
+}
+
+/// Converts our `2000-01-01 12:00 UTC` format to RFC3339 without the timezone suffix
+fn convert_legacy_last_updated(s: &str) -> Result<String> {
+    // let timestamp = PrimitiveDateTime::parse(
+    //     s,
+    //     format_description!("[year]-[month]-[day] [hour]:[minute] UTC"),
+    // )?
+    // .assume_utc();
+    // let mut converted = timestamp.format(&Rfc3339)?;
+    // converted.pop();
+    // Ok(converted)
+
+    let re = lazy_regex!(r"(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}) UTC");
+    let caps = re.captures(s).context("Invalid last updated")?;
+    let date = caps.get(1).context("Invalid last updated")?.as_str();
+    let time = caps.get(2).context("Invalid last updated")?.as_str();
+    Ok(format!("{date}T{time}:00"))
 }
 
 fn rfc3339_to_millis(s: &str) -> u64 {

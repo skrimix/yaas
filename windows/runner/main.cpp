8c3561d035871016a5cdb171fdf8823c2bfe9e87
skrimix <skrimix.git@fastmail.org>
2026-08-14 15:02:14 +0500

    Fix Windows console output

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,6 +1,10 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
+#include <flutter_windows.h>
+#include <stdio.h>
 #include <windows.h>
+
+#include <iostream>
 
 #include "flutter_window.h"
 #include "utils.h"
@@ -9,7 +13,14 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
-  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+  if (::AttachConsole(ATTACH_PARENT_PROCESS)) {
+    FILE *unused;
+    freopen_s(&unused, "CONOUT$", "w", stdout);
+    freopen_s(&unused, "CONOUT$", "w", stderr);
+
+    std::ios::sync_with_stdio();
+    FlutterDesktopResyncOutputStreams();
+  } else if (::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
 
